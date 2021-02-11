c23f407a0f89f26af701a9ee2629ba6666dd9d8b
felex <engineerfelex@gmail.com>
2021-02-12 00:16:48 +0300

    lost in the hood

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:substation/services/sign_in.dart';
 import 'dashboard.dart';
-import 'reading_screen.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -10,32 +8,24 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
   // String time = 'loading';
-  void setUpWorldTime() async {
-     signInWithGoogle()
-        .whenComplete(() {
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) {
-            // return null;
-            return DashBoard();
-          },
-        ),
-      );
-    });
+  void loadLogin() async {
+    // signInWithGoogle().whenComplete(() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          // return null;
+          return DashBoard();
+        },
+      ),
+    );
   }
-
-
-
-
-
-
 
   @override
   void initState() {
     super.initState();
     // print('object in Init State');
     // worldTime.getTime();
-    setUpWorldTime();
+    loadLogin();
   }
 
   @override
@@ -44,9 +34,8 @@ class _LoadingState extends State<Loading> {
       backgroundColor: Colors.indigo[100],
       body: SafeArea(
         child: Center(
-             child: CircularProgressIndicator(),
-            ),
-
+          child: CircularProgressIndicator(),
+        ),
       ),
     );
   }
